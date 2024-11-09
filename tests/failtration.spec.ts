95d0bec977726fdcc641ef3a9f8ce3c7d464b2e3
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2024-11-09 12:38:51 +0200

    add check one selected  type

--- tests/failtration.spec.ts
+++ tests/failtration.spec.ts
@@ -9,19 +9,20 @@ test.describe("Filtration methods testing", () => {
             waitUntil: "domcontentloaded"
         });
     });
-    test("Select type", async ({ page }) => {
+    test("Check selected type by value", async ({ page }) => {
         const mainPageFiltration = new MainPageFiltration(page);
-
         await mainPageFiltration.selectType("Тип транспорту", "6");
         const selectedType = await mainPageFiltration.getSelectedType("Тип транспорту");
-        expect(selectedType).toBe("6") 
-
+        expect(selectedType).toBe("6");
     })
-    test("Check all available type type", async ({ page }) => {
+    test("Check all available type", async ({ page }) => {
         const dropDownList = page.locator("#categories > option");
-        await expect(dropDownList).toHaveText(["Будь-який", "Легкові", "Мото", "Вантажівки", "Причепи", "Спецтехніка", "Сільгосптехніка", "Автобуси", "Водний транспорт", "Повітряний транспорт", "Автобудинки"])
-        
-
+        await expect(dropDownList).toHaveText(["Будь-який", "Легкові", "Мото", "Вантажівки", "Причепи", "Спецтехніка", "Сільгосптехніка", "Автобуси", "Водний транспорт", "Повітряний транспорт", "Автобудинки"]);
+    })
+    test("Check selected type by text", async ({ page }) => {
+        const mainPageFiltration = new MainPageFiltration(page);
+        await mainPageFiltration.selectType("Тип транспорту", "Легкові");
+        await expect(page.locator('#categories option[selected="selected"]')).toHaveText('Легкові');   
     })
 })
 
