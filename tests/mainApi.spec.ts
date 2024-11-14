79a6248cc8df08df88b83f4ee3908acf55f4a471
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2024-11-14 21:25:06 +0200

    cheking github action

--- tests/mainApi.spec.ts
+++ tests/mainApi.spec.ts
@@ -1,7 +1,6 @@
 // @ts-check
 import { test } from '@playwright/test';
 import { MainPageApiTesting } from '../pages/api/mainPageApi';
-import { linkData } from '../data/mainPageTestData';
 
 
 test.describe("Main page API testing", () => {
@@ -9,11 +8,6 @@ test.describe("Main page API testing", () => {
     test("Check response status", async ({ request}) => {
         const mainPageApiTest = new MainPageApiTesting(request);
 
-
-        for (const value of linkData)  {
-            await mainPageApiTest.getStatus(value.link, 200);
-        }
-        
     })
 
 })
