fdde583b15fbc6fde6919f677192b8091a728c09
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2024-11-15 20:09:51 +0200

    verify 200 OK for main page

--- tests/mainPageApi.spec.ts
+++ tests/mainPageApi.spec.ts
@@ -7,5 +7,8 @@ test.describe("Main page API testing", () => {
 
     test("Check response status", async ({ request}) => {
         const mainPageApiTest = new MainPageApiTesting(request);
+
+        await mainPageApiTest.getStatus("https://novaposhta.ua/", 200);
+        await mainPageApiTest.getStatus("https://novapost.com/uk-ua/", 200);
     })
 })
