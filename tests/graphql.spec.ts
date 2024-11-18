98cb13f124c54c34ce8b5362910e0d8fbbd324a8
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2024-11-18 16:58:08 +0200

    add graphql mutation test

--- tests/graphql.spec.ts
+++ tests/graphql.spec.ts
@@ -53,4 +53,46 @@ test.describe("GraphQL API testing", () => {
         expect(firstFilm.speciesConnection.species.length).toBeGreaterThanOrEqual(0);
         console.log(firstFilm)
     });
+
+    test("Create a new user", async ({ request }) => {
+        const mutation = `
+          mutation AddUser($name: String!, $gender: String!, $email: String!, $status: String!) {
+            createUser(input: { name: $name, gender: $gender, email: $email, status: $status }) {
+              user {
+                id
+                name
+                email
+                gender
+                status
+              }
+            }
+          }
+        `;
+    
+        const variables = {
+          name: "Obi-Wan Kenobi",
+          gender: "male",
+          email: `obiwankenobi.${Date.now()}@example.com`, // Unique email
+          status: "active",
+        };
+    
+        const response = await request.post("https://gorest.co.in/public/v2/graphql", {
+          headers: {
+            Authorization: `Bearer 60af87747e81d4ebdfcff9e7664127165c2dac89be65209be8fcc52c9b6e344c`,
+          },
+          data: {
+            query: mutation,
+            variables,
+          },
+        });
+    
+        const result = await response.json();
+        console.log(result.data.createUser);
+    
+        expect(response.ok()).toBeTruthy();
+        expect(result.data.createUser.user.name).toBe(variables.name);
+        expect(result.data.createUser.user.email).toBe(variables.email);
+        expect(result.data.createUser.user.gender).toBe(variables.gender);
+        expect(result.data.createUser.user.status).toBe(variables.status);
+      });
 });
