28fa4c12034f6e1877f28d9df59efb694f34d1c0
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2024-11-29 19:49:08 +0200

    check users object have all property

--- tests/apigorest.spec.ts
+++ tests/apigorest.spec.ts
@@ -9,6 +9,21 @@ test.describe("Gorest API testing", () => {
 		const posts = await response.json();
 
 		expect(response.status()).toBe(200);
-		expect(posts.length).toBe(10);
+		expect(posts).toHaveLength(10);
+	});
+
+	test("Get list of users", async ({ request }) => {
+		const response = await request.get(
+			"https://gorest.co.in/public/v2/users"
+		);
+		const users = await response.json();
+
+		expect(response.status()).toBe(200);
+		expect(users).toHaveLength(10);
+		expect(users[0]).toHaveProperty("id");
+		expect(users[0]).toHaveProperty("name");
+		expect(users[0]).toHaveProperty("email");
+		expect(users[0]).toHaveProperty("gender");
+		expect(users[0]).toHaveProperty("status");
 	});
 });
