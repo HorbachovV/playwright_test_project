cfb9e1f72e5dfc4d0d1392b4b376779263960e74
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2024-12-02 14:44:57 +0200

    delete comments

--- pages/api/gorestapi.ts
+++ pages/api/gorestapi.ts
@@ -1,24 +1,25 @@
 import { expect } from "playwright/test";
 import { RestApi } from "../../components/api/restapi";
 
-
 export class GetUsersData {
 	private readonly client: RestApi;
-  
+
 	constructor(client: RestApi) {
-	  this.client = client;
+		this.client = client;
 	}
-  
-	async getAllPosts(url: string, expectedStatus: number, expectedLength: number) {
-	  const response = await this.client.getApi(url);
-  
-	  // Перетворюємо відповідь у JSON
-	  const data = await response.json();
-  
-	  // Перевірки
-	  expect(response.status()).toBe(expectedStatus);
-	  expect(data).toHaveLength(expectedLength);
-  
-	  return data;
+
+	async getAllPosts(
+		url: string,
+		expectedStatus: number,
+		expectedLength: number
+	) {
+		const response = await this.client.getApi(url);
+
+		const data = await response.json();
+
+		expect(response.status()).toBe(expectedStatus);
+		expect(data).toHaveLength(expectedLength);
+
+		return data;
 	}
-  }
+}
