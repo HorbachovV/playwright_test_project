91798553bed2f5a11bd61befef8230b7dac97b3f
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2024-12-18 14:06:12 +0200

    add test for retrieving user posts

--- pages/api/gorestapipost.ts
+++ pages/api/gorestapipost.ts
@@ -1,2 +1,21 @@
 import { expect } from "playwright/test";
 import { RestApi } from "../../components/api/restapi";
+
+export class GetUserPosts {
+	private readonly client: RestApi;
+
+	constructor(client: RestApi) {
+		this.client = client;
+	}
+
+	async getData(url: string, expectedStatus: number, expectedLength: number) {
+		const response = await this.client.getApi(url);
+
+		const data = await response.json();
+
+		expect(response.status()).toBe(expectedStatus);
+		expect(data).toHaveLength(expectedLength);
+
+		return data;
+	}
+}
--- tests/apipostgorest.spec.ts
+++ tests/apipostgorest.spec.ts
@@ -1,2 +1,25 @@
 // @ts-check
 import { test, expect, APIRequestContext } from "@playwright/test";
+import { RestApi } from "../components/api/restapi";
+import { GetUserPosts } from "../pages/api/gorestapipost";
+import { GOREST_DATA } from "../data/gorestData";
+
+const token = GOREST_DATA.token;
+const mainUrl = GOREST_DATA.url;
+
+test.describe("REST API Tests: Posts", () => {
+	let getUserPosts: GetUserPosts;
+
+	test.beforeEach(async ({ request }: { request: APIRequestContext }) => {
+		const restApi = new RestApi(request, token);
+		getUserPosts = new GetUserPosts(restApi);
+	});
+
+	test("Get list of users", async () => {
+		const url = `${mainUrl}posts`;
+		const status = 200;
+		const expectedLength = 10;
+
+		await getUserPosts.getData(url, status, expectedLength);
+	});
+});
